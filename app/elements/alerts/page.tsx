ba9a2eca6bb6502d8898ab8045255b4b54b2403f
idimetrix <selikhov.dmitrey@gmail.com>
2024-09-25 04:45:31 -0400

    -

--- app/elements/alerts/page.tsx
+++ app/elements/alerts/page.tsx
@@ -5,11 +5,13 @@ import {
   faCircleCheck,
   faCircleExclamation,
   faCircleInfo,
+  faClose,
   faTriangleExclamation,
 } from "@fortawesome/pro-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { Metadata } from "next"
 import Link from "next/link"
+import { useCallback } from "react"
 import { Alert } from "../../../components/Alert"
 import { Card } from "../../../components/Card"
 import { Headline } from "../../../elements/Headline"
@@ -43,6 +45,8 @@ const Icons: Record<VARIANTS, IconProp> = {
 }
 
 export default function Page() {
+  const handleDismiss = useCallback((variant: string) => () => {}, [])
+
   return (
     <section className="flex w-full flex-col gap-6">
       <Headline title="Alerts">
@@ -78,8 +82,11 @@ export default function Page() {
         <Card header="Dismissing">
           <div className="flex flex-col gap-3">
             {Object.entries(VARIANTS).map(([key, value]) => (
-              <Alert className="text-left text-sm" key={key} variant={value}>
+              <Alert className="pr-10 text-left text-sm" key={key} variant={value}>
                 You should check in on some of those fields below.
+                <button className="absolute right-2 top-2 flex size-7 items-center justify-center rounded bg-transparent bg-opacity-10 text-inherit hover:bg-inherit">
+                  <FontAwesomeIcon icon={faClose} className="size-4" />
+                </button>
               </Alert>
             ))}
           </div>
@@ -87,7 +94,19 @@ export default function Page() {
       </div>
 
       <div className="flex gap-6">
-        <Card header="Acon Alerts">
+        <Card header="Content">
+          <div className="flex flex-col gap-3">
+            <Alert className="flex flex-col items-start gap-1.5 text-left text-sm" variant="success">
+              <div className="text-xl font-bold">Well done!</div>
+              Aww yeah, you successfully read this important alert message. This example text is going to run a bit
+              longer so that you can see how spacing within an alert works with this kind of content.
+              <hr className="my-1.5 w-full border-inherit" />
+              Whenever you need to, be sure to use margin utilities to keep things nice and tidy.
+            </Alert>
+          </div>
+        </Card>
+
+        <Card header="Icons">
           <div className="flex flex-col gap-3">
             {Object.entries(VARIANTS).map(([key, value]) => (
               <Alert className="flex items-center gap-1.5 text-left text-sm" key={key} variant={value}>
--- components/Alert.tsx
+++ components/Alert.tsx
@@ -9,7 +9,7 @@ const alert = tv({
     variant: {
       primary: "text-primary-800 bg-primary-500 border-primary-500",
       secondary: "text-secondary-800 dark:text-secondary-200 bg-secondary-500 border-secondary-500",
-      success: "text-success-800 bg-success-500 border-success-500",
+      success: "text-success-800 bg-success-500 border-success-500 bg-opacity-10 border-opacity-10",
       danger: "text-danger-800 bg-danger-500 border-danger-500",
       warning: "text-warning-800 bg-warning-500 border-warning-500",
       info: "text-info-800 bg-info-500 border-info-500",
@@ -21,7 +21,9 @@ const alert = tv({
   },
 })
 
-interface AlertVariants extends VariantProps<typeof alert> {
+export interface AlertVariants extends VariantProps<typeof alert> {}
+
+export interface AlertProps extends HTMLAttributes<HTMLButtonElement | HTMLAnchorElement>, AlertVariants {
   hover?: boolean
   href?: string
   target?: HTMLAttributeAnchorTarget
@@ -29,8 +31,6 @@ interface AlertVariants extends VariantProps<typeof alert> {
   disabled?: boolean
   type?: "submit" | "reset" | "button" | undefined
 }
-
-export interface AlertProps extends HTMLAttributes<HTMLButtonElement | HTMLAnchorElement>, AlertVariants {}
 
 export const Alert = forwardRef<HTMLAnchorElement | HTMLButtonElement, AlertProps>(function Alert(
   { hover, href, target, rounded, variant = "primary", className, children, disabled, type, ...rest }: AlertProps,
